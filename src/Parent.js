fae72aa0f733d1849cd3ebd08631f2b1857f701f
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-01 14:22:59 +0300

    Assignment done

--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,19 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childrenColor, sectChildrenColor] = useState("#FFF");
+
+  //Setting function to update state in the parent component to run the div when clicked
+  function handleChangeColor(newChildColor) {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    sectChildrenColor(newChildColor);
+  }
 
   return (
-    <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+    <div className="parent" style={{ backgroundColor: color  }}>
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
+      <Child color={childrenColor} onChangeColor={handleChangeColor}/>
     </div>
   );
 }
